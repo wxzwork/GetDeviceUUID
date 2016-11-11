9703e6578ea2e02a35c19c050f41c704fd17f939
woshipm <wxz_work@163.com>
2016-11-11 15:00:28 +0800

    Initial Commit

---
+++ 获取设备的UUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  获取设备的UUID
+//
+//  Created by WOSHIPM on 16/11/11.
+//  Copyright © 2016年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 获取设备的UUID/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  获取设备的UUID
+//
+//  Created by WOSHIPM on 16/11/11.
+//  Copyright © 2016年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
